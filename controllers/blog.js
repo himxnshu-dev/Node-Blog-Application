f9e39d8e5c6f92c39fcf90d53773143262560a91
himanshu <khatri.hk18@gmail.com>
2025-10-26 19:16:56 +0530

    feat(blog): Implement comment functionality with form and display on blog page

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,40 +1,62 @@
-const Blog = require("../models/blog")
-const User = require("../models/user")
+const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 
 const handleGetBlogPage = (req, res) => {
-    return res.render("blog", {
-        user: req.user
-    })
-}
+  return res.render("blog", {
+    user: req.user,
+  });
+};
 
 const handlePostBlogs = async (req, res) => {
-    // console.log(req.file)
-    const { title, body } = req.body;
-    const blog = await Blog.create({
-        title,
-        content: body,
-        coverImageURL: req.file ? `/uploads/${req.file.filename}` : null,
+  // console.log(req.file)
+  const {title, body} = req.body;
+  const blog = await Blog.create({
+    title,
+    content: body,
+    coverImageURL: req.file ? `/uploads/${req.file.filename}` : null,
+    createdBy: req.user._id,
+  });
+
+  console.log(
+    "The blog has been successfully added to the DB with title:",
+    blog.title
+  );
+
+  return res.redirect(`/blog/${blog._id}`);
+};
+
+const handleGetUserBlogInfo = async (req, res) => {
+  const blogId = req.params.blogId;
+  // console.log("Blog ID:", blogId)
+
+  const blog = await Blog.findById(blogId).populate("createdBy");
+  console.log("Blog title:", blog.title);
+
+  const comments = await Comment.find({blogId}).populate("createdBy")
+//   console.log("Comment object:", comments)
+
+  return res.render("currentBlog", {
+    blog: blog,
+    user: req.user,
+    comments: comments
+  });
+};
+
+const handlePostCommentOnBlog = async (req, res) => {
+    const {content} = req.body;
+
+    await Comment.create({
+        comment: content,
+        blogId: req.params.blogId,
         createdBy: req.user._id
     })
 
-    console.log("The blog has been successfully added to the DB with title:", blog.title)
+    return res.status(200).redirect(`/blog/${req.params.blogId}`)
+};
 
-    return res.redirect(`/blog/${blog._id}`)
-}
-
-const handleGetUserBlogInfo = async (req, res) => {
-    const blogId = req.params.blogId
-    // console.log("Blog ID:", blogId)
-
-    const blog = await Blog.findById(blogId).populate("createdBy")
-    console.log("Blog title:", blog.title)
-
-
-
-    return res.render("currentBlog", {
-        blog: blog,
-        user:req.user,
-    })
-}
-
-module.exports = { handleGetBlogPage, handlePostBlogs, handleGetUserBlogInfo }
+module.exports = {
+  handleGetBlogPage,
+  handlePostBlogs,
+  handleGetUserBlogInfo,
+  handlePostCommentOnBlog,
+};
--- routes/blog.js
+++ routes/blog.js
@@ -1,13 +1,19 @@
 const {Router} = require("express");
 const router = Router();
-const {handleGetBlogPage, handlePostBlogs, handleGetUserBlogInfo} = require("../controllers/blog");
-const {authenticateUserToken} = require("../middlewares/auth.middleware");
+const {
+  handleGetBlogPage,
+  handlePostBlogs,
+  handleGetUserBlogInfo,
+  handlePostCommentOnBlog,
+} = require("../controllers/blog");
 const upload = require("../middlewares/multer.middleware");
 
 router.get("/add-new", handleGetBlogPage);
 
 router.post("/", upload.single("image"), handlePostBlogs);
 
-router.get("/:blogId", handleGetUserBlogInfo)
+router.get("/:blogId", handleGetUserBlogInfo);
+
+router.post("/comment/:blogId", handlePostCommentOnBlog);
 
 module.exports = router;
